570b9c27de2dd12884fff367f7466ac4bef72f93
deepspacediver <luka.koscik@gmail.com>
2022-12-19 18:05:28 +0100

    CVSection component

---
+++ src/components/CVCreator/CVSection.js
@@ -0,0 +1,52 @@
+import { Component } from "react";
+import uniqid from "uniqid";
+
+class CVSection extends Component {
+  constructor(props) {
+    super(props);
+    const { FormComponent } = this.props;
+    this.state = {
+      formList: [{ form: <FormComponent />, id: uniqid() }],
+    };
+  }
+  addForm = (FormComponent) => {
+    const updatedFormList = [
+      ...this.state.formList,
+      { form: FormComponent, id: uniqid() },
+    ];
+    this.setState({
+      formList: updatedFormList,
+    });
+  };
+
+  deleteForm = (e) => {
+    const id = e.target.dataset.id;
+    console.log(id)
+    const updatedFormList = this.state.formList.filter((el) => el.id !== id);
+    this.setState({
+      formList: updatedFormList,
+    });
+  };
+
+  render() {
+    const { formList } = this.state;
+    const { FormComponent } = this.props;
+    return (
+      <div>
+        {formList.map((obj) => {
+          return (
+            <div key={obj.id}>
+              {obj.form}
+              <button data-id={obj.id} onClick={this.deleteForm}>
+                Delete me
+              </button>
+            </div>
+          );
+        })}
+        <button onClick={() => this.addForm(<FormComponent />)}>AddForm</button>
+      </div>
+    );
+  }
+}
+
+export default CVSection;
