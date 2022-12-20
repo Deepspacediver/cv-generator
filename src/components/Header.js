a0ab7bde102148cce57c9a4201fb7ba98fc846fb
deepspacediver <luka.koscik@gmail.com>
2022-12-20 21:17:08 +0100

    add header template

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>CV Creator</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
