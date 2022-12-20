33d79368519d1fad997a54bbda37298c4bd2cf05
deepspacediver <luka.koscik@gmail.com>
2022-12-20 21:16:50 +0100

    footer template

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>Copyright DeepSpaceDiver</p>
+        {/* <img src="" alt="" /> */}
+      </footer>
+    );
+  }
+}
+
+export default Footer;
