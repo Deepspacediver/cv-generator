d39457d84ca0bf7f6a815c3e42fcabe9b5a96581
deepspacediver <luka.koscik@gmail.com>
2022-12-20 12:16:40 +0100

    initial preview of cv

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./index.css";
-
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from "react";
+import GeneralForm from "./components/CVCreator/specificForms/GeneralForm";
+import CVSection from "./components/CVCreator/CVSection";
+import EducationalForm from "./components/CVCreator/specificForms/EducationForm";
+import WorkForm from "./components/CVCreator/specificForms/WorkForm";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CVSection FormComponent={GeneralForm} />
+        <CVSection FormComponent={EducationalForm} />
+        <CVSection FormComponent={WorkForm} />
+      </div>
+    );
+  }
 }
 
 export default App;
