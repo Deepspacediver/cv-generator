6b26c8e9db611a0c3423940d1ae0c82007016875
deepspacediver <luka.koscik@gmail.com>
2022-12-21 21:58:54 +0100

    app template

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import "./index.css";
 import React, { Component } from "react";
-import GeneralForm from "./components/CVCreator/specificForms/GeneralForm";
-import CVSection from "./components/CVCreator/CVSection";
-import EducationalForm from "./components/CVCreator/specificForms/EducationForm";
-import WorkForm from "./components/CVCreator/specificForms/WorkForm";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 class App extends Component {
   render() {
     return (
-      <div>
-        <CVSection FormComponent={GeneralForm} />
-        <CVSection FormComponent={EducationalForm} />
-        <CVSection FormComponent={WorkForm} />
-      </div>
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
     );
   }
 }
